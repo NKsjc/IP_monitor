2f58dda72924bf0cd55952756baa421e4193af3b
NKsjc <1624285520@qq.com>
2015-10-16 20:40:43 +0800

    C#网络编程IP流量监控

---
+++ ip_monitor/ip_monitor/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace ip_monitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPHostEntry IPentry = Dns.GetHostEntry(Dns.GetHostName());
+            string ipList = IPentry.AddressList[0].ToString();
+            System.Net.IPAddress[] addressList = Dns.GetHostByName(Dns.GetHostName()).AddressList;
+            if (addressList.Length > 1)
+            {
+                lblLIP.Text = addressList[0].ToString();
+                lblSIP.Text = addressList[1].ToString();
+            }
+            else
+            {
+                lblLIP.Text = addressList[0].ToString();
+                lblSIP.Text = ipList;
+            }
+        }
+    }
+}
